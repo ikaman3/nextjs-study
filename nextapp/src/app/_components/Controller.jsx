2e36fcc4a09bf9a46aa5768a6701b819d8ac2926
ikaman3 <yjhama1532@gmail.com>
2024-03-02 23:31:15 +0900

    nextapp: Linting Error 모두 잡음

--- nextapp/src/app/_components/Controller.jsx
+++ nextapp/src/app/_components/Controller.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 export default function Controller({ buttonNumbers, onCountChange }) {
   // 부모 컴포넌트로부터 buttonNumbers 배열을 받아 원소의 개수만큼 버튼을 생성
   return (
@@ -17,4 +19,14 @@ export default function Controller({ buttonNumbers, onCountChange }) {
       })}
     </div>
   )
+}
+
+Controller.propTypes = {
+  buttonNumbers: PropTypes.arrayOf,
+  onCountChange: PropTypes.func,
+}
+
+Controller.defaultProps = {
+  buttonNumbers: [],
+  onCountChange: () => {},
 }
--- nextapp/src/app/_components/Viewer.jsx
+++ nextapp/src/app/_components/Viewer.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 export default function Viewer({ count }) {
   return (
     <div>
@@ -5,4 +7,12 @@ export default function Viewer({ count }) {
       <h2>{count}</h2>
     </div>
   )
+}
+
+Viewer.propTypes = {
+  count: PropTypes.number,
+}
+
+Viewer.defaultProps = {
+  count: 0,
 }
