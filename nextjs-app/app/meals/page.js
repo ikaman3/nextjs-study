a3d3f7b0462d41a69f4f55b99677600bbb9ad980
ikaman3 <yjhama1532@gmail.com>
2024-03-31 23:17:04 +0900

    next-app: Meals 데이터와 크기가 불분명한 이미지 출력, better-sqlite3 의존성 설치

--- nextjs-app/app/meals/page.js
+++ nextjs-app/app/meals/page.js
@@ -1,3 +1,23 @@
+import Link from 'next/link'
+import MealsGrid from '@/components/meals/meals-grid'
+
+import classes from './page.module.css'
+
 export default function MealsPage() {
-    return <h1>This is meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!!!</p>
+      </header>
+      <p className={classes.cta}>
+        <Link href="/meals/share">Share Your Favorite Recipe</Link>
+      </p>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  )
 }
