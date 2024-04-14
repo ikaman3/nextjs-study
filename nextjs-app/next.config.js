8e62fc0bdc61b526fdf488d42d941943d9771d6c
ikaman3 <yjhama1532@gmail.com>
2024-04-14 20:16:13 +0900

    nextjs-app: 도메인으로 배포하기 위해 prefix 추가

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: 'https://ikaman.duckdns.org/nextjs-app',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
