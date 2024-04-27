f70e924cf6c51ad37aa11e83285401dae6ea952f
ikaman3 <yjhama1532@gmail.com>
2024-04-27 23:13:48 +0900

    nextjs-app: static metadata

--- nextjs-app/app/meals/page.js
+++ nextjs-app/app/meals/page.js
@@ -6,6 +6,11 @@ import { getMeals } from '@/lib/meals'
 
 import classes from './page.module.css'
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+}
+
 async function Meals() {
   const meals = await getMeals()
 
@@ -17,15 +22,21 @@ export default function MealsPage() {
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created <span className={classes.highlight}>by you</span>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!!!</p>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and
+          fun!!!
+        </p>
       </header>
       <p className={classes.cta}>
         <Link href="/meals/share">Share Your Favorite Recipe</Link>
       </p>
       <main className={classes.main}>
-        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
           <Meals />
         </Suspense>
       </main>
