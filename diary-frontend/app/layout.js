4fae1bb7bc008a0f1d1525a1538cec5d22eaafb6
ikaman3 <yjhama1532@gmail.com>
2024-05-05 03:19:49 +0900

    diary-frontend: 이미지 추가

--- diary-frontend/app/layout.js
+++ diary-frontend/app/layout.js
@@ -1,11 +1,13 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+})
 
 export const metadata = {
-  title: "Ikaman's Portfolio",
-  description: 'Next.js를 사용한 프론트엔드 프로젝트',
+  title: 'Emotion Diary',
+  description: 'Next.js를 사용한 감정 일기장 프론트엔드 프로젝트',
 }
 
 export default function RootLayout({ children }) {
